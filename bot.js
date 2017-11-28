33af2188355815c4088103642fe9850e722a30d2
Samiirosmaan112 <33898442+Samiirosmaan112@users.noreply.github.com>
2017-11-28 19:18:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
-  client.user.setGame("d!help", "https://go.twitch.tv/pallavbs"); // a game
+  client.user.setGame("phelp", "https://go.twitch.tv/pallavbs"); // a game
   client.user.setStatus("online"); // Set status to DnD
 
 });
@@ -33,39 +33,43 @@ client.on("guildDelete", guild => {
 
 client.on('guildMemberAdd', member => {
   // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find('name', 'member-log');
   // Do nothing if the channel wasn't found on this server
   if (!channel) return;
   // Send the message, mentioning the member
   channel.send(`A new survivor has been spotted, ${member}`);
 });
 
+
 client.on("message", (message) => {
   // This event will run on every single message received, from any channel or DM.
-
+  
   // It's good practice to ignore other bots. This also makes your bot ignore itself
   // and not get into a spam loop (we call that "botception").
   if(message.author.bot) return;
   console.log(message.guild.name)
+  if(!message.member.roles.some(r=>["Administrator", "Moderator", "Admin"].includes(r.name)) )
         return ;  // Also good practice to ignore any message that does not start with our prefix, 
   // which is set in the configuration file.
   if(message.content.indexOf(config.prefix) !== 0) return;
-
+  
   // Here we separate our "command" name, and our "arguments" for the command. 
   // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
   // command = say
   // args = ["Is", "this", "the", "real", "life?"]
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
-
+  
   // Let's go with a few common example commands! Feel free to delete or change those.
-
+  
   if(command === "ping") {
     // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
     // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
     message.channel.send("Ping?").then((msg)=>{
     msg.edit('Pong!')
     })
-
+                                       }
+  
   if(command === "say") {
     // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
     // To get the "message" itself we join the `args` back into a string with spaces: 
@@ -84,14 +88,14 @@ client.on("message", (message) => {
     else{
        message.channel.send(sayMessage);}
   }
-
+  
   if(command === "kick") {
     // This command must be limited to mods and admins. In this example we just hardcode the role names.
     // Please read on Array.some() to understand this bit: 
     // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
     if(!message.member.roles.some(r=>["Administrator", "Moderator", "Admin"].includes(r.name)) )
       return ;
-
+    
     // Let's first check if we have a member and if we can kick them!
     // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
     let member = message.mentions.members.first();
@@ -99,25 +103,25 @@ client.on("message", (message) => {
       return message.reply("Please mention a valid member of this server");
     if(!member.kickable) 
       return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
-
+    
     // slice(1) removes the first part, which here should be the user mention!
     let reason = args.slice(1).join(' ');
     if(!reason)
       return message.reply("Please indicate a reason for the kick!");
-
+    
     // Now, time for a swift kick in the nuts!
     member.kick(reason)
       .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
     message.channel.send(`${member.user.tag} has been kicked by ${message.author} because: ${reason}`);
 
   }
-
+  
   if(command === "ban") {
     // Most of this command is identical to kick, except that here we'll only let admins do it.
     // In the real world mods could ban too, but this is just an example, right? ;)
     if(!message.member.roles.some(r=>["Administrator", "Admin"].includes(r.name)) )
       return ;
-
+    
     let member = message.mentions.members.first();
     if(!member)
       return message.reply("Please mention a valid member of this server");
@@ -127,29 +131,60 @@ client.on("message", (message) => {
     let reason = args.slice(1).join(' ');
     if(!reason)
       return message.reply("Please indicate a reason for the ban!");
-
+    
     member.ban(reason)
       .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
     message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
   }
-
+  
   if(command === "purge" || command === "prune") {
   // This command removes all messages from all users in the channel, up to 100.
-    
+      
     // This command must be limited to mods and admins. In this example we just hardcode the role names.
-    
+
     if(!message.member.roles.some(r=>["Administrator", "Moderator", "Admin"].includes(r.name)) )
       return ;
-    
+      
     // get the delete count, as an actual number.
     const deleteCount = parseInt(args[0], 10) + 1;
     message.channel.fetchMessages({limit: deleteCount}).then(messages => message.channel.bulkDelete(messages));
 
    }
-
+  
+  if(command === "eyes") {
+      //This command send emoji the looks left and read
+      
+      message.channel.send("👀").then((msg)=>{
+      var i = 0;
+      while (i < 50) {
+      msg.edit('<:eyesflipped:374082394816249856>')
+      msg.edit("👀");
+      i++;
+      }
+      })
+    
+}                          
+  
+    if (command === "qwe") {
+      var i = 0;
+      while (i < 50) {
+      message.channel.send(50-i)
+      i++;
+      }
+}
+  if (command === "avatar" || command === 'avi') {
+    let member = message.mentions.members.first();
+    
+  // Send the user's avatar URL
+    message.reply(member.avatarURL);
+  
+}
    if (command === "help") {
+    message.delete(); 
     message.author.send("**Prefix:** `p`\n" +
   "__**Commands**__:\nping\nsay\nkick\nban\npurge\neyes\navatar ");}
 });
 
 client.login(process.env.BOT_TOKEN);
+
+  
